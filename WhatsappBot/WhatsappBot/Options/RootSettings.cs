0ba773259aff00371fed01d05fbe10c2f9fee737
Danielike <dredirect@duck.com>
2026-07-15 19:03:11 +0200

    Add evolution api fields in appsettings

--- WhatsappBot/WhatsappBot/Options/RootSettings.cs
+++ WhatsappBot/WhatsappBot/Options/RootSettings.cs
@@ -1,29 +1,35 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
 namespace WhatsappBot.Options;
 
 public class RootSettings
 {
-    public Logging Logging { get; set; }
-    public ConfigurationOptions ConfigurationOptions { get; set; }
-    public string AllowedHosts { get; set; }
+    public Logging Logging { get; set; } = new  Logging();
+    public ConfigurationOptions ConfigurationOptions { get; set; } = new ConfigurationOptions();
+    public string AllowedHosts { get; set; } = string.Empty;
 }
 
 public class Logging
 {
-    public LogLevel LogLevel { get; set; }
+    public LogLevel LogLevel { get; set; } = new LogLevel();
 }
 
 public class LogLevel
 {
-    public string Default { get; set; }
-    public string Microsoft_AspNetCore { get; set; }
+    public string Default { get; set; } = string.Empty;
+    public string MicrosoftAspNetCore { get; set; } = string.Empty;
 }
 
 public class ConfigurationOptions
 {
-    public required string ForbiddenGenres { get; set; }
-    public required string FlaresolverrUrl { get; set; }
-    public required string SiteUrl { get; set; }
-    public required string NameSelector { get; set; }
-    public required string GenresSelector { get; set; }
+    public string ForbiddenGenres { get; set; } = string.Empty;
+    public string FlaresolverrUrl { get; set; } = string.Empty;
+    public string SiteUrl { get; set; } = string.Empty;
+    public string NameSelector { get; set; } = string.Empty;
+    public string GenresSelector { get; set; } = string.Empty;
+    public string EvolutionApiUrl { get; set; } = string.Empty;
+    public string EvolutionApiEndpoint { get; set; } = string.Empty;
+    public string EvolutionApiInstance { get; set; } = string.Empty;
+    public string EvolutionApiKey { get; set; } = string.Empty;
 }
 
